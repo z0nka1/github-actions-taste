bb531068d053089ac2448bf1dbbcc989a9eae670
z0nka1 <liang3075435@163.com>
2019-10-03 16:07:58 +0800

    chore:edit greeting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Github Actions build and deploy this App.
         </p>
         <a
           className="App-link"
