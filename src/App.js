0d722ce68d485c5038474a4b6e3347fe4ee267fc
梁宗凯 <liang3075435@163.com>
2020-06-15 21:10:34 +0800

    Revert "page"
    This reverts commit a3c7aead39d848274d8d6273cfa154411c6ca12b.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import MyConcerns from './components/MyConcerns/MyConcerns';
-import MyService from './components/MyService/MyService';
-import Banner from './components/Banner/Banner';
-import dance from './assets/dance.png';
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <div className="nav"></div>
-        <div className="wave_1"></div>
-        <div className="wave_2"></div>
-        <div className="search">
-          <img className="dance" src={dance} alt="dance" />
-        </div>
-        <div className="banner">
-          <Banner />
-        </div>
-      </div>
-      <div className="body">
-        <div className="content">
-          <div className="pos-relative my_concerns_container">
-            <MyConcerns />
-          </div>
-          <div className="pos-relative my_service_container">
-            <MyService />
-          </div>
-          <div className="ad_container"></div>
-          <div className="news_container"></div>
-        </div>
-      </div>
-      <div className="footer"></div>
-      <div className="size_bar"></div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Github Actions build and deploy this App.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
